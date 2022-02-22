53d4f7a1ac2ac8b58d068a09afac9f41a5c4e637
Michael Breuer <michael.breuer@aspirecreative.co.uk>
2022-02-23 00:35:26 +0800

    Test writing and contract improvements

--- test/TestMyEpicNFT.js
+++ test/TestMyEpicNFT.js
@@ -1,5 +1,6 @@
 const { assert, expect } = require("chai");
 const { ethers } = require("hardhat");
+const base64 = require('base-64')
 
 describe("MyEpicNFT", function () {
   let contract;
@@ -16,6 +17,26 @@ describe("MyEpicNFT", function () {
       return assert.isTrue(true);
     });
   });
+
+  describe("Make an NFT", () => {
+    it("Contract has the right name", async () => {
+      let name = await contract.name()
+      return assert.equal(name, "MMA Fighters");
+    });
+
+    it("Contract has the right symbol", async () => {
+      let symbol = await contract.symbol()
+      return assert.equal(symbol, "DJMMA");
+    });
+
+    it("Owner mints an NFT", async () => {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      let jsonData1 = base64.encode('{"description": "What To Do on Your Day Off", "external_url": "add_external_url_here", "image": "image_url_here", "name": "Ferris Bueller"}');
+      await contract.connect(owner).makeAnEpicNFT(jsonData1);
+      let verify = await contract.balanceOf(owner.address);
+      return assert.equal(verify, 1);
+    });
+  })
   
 });
 
